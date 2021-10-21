b342572c86e2d2b664f1b02fde046cea3a188306
Ignacio Todeschini <ignacio.todeschini@gmail.com>
2021-10-21 12:09:23 -0300

    HomePage with post

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [nickname, setNickname] = useState([]);
+
+  const handleNickname = event => {
+    setNickname(event.target.value);
+  };
+
+  const sendNicknameToBase = nick => {
+    fetch('http://localhost:3000/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(nick),
+    }).then(response => response.json());
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,9 +23,19 @@ function App() {
           <section>
             <form>
               <p>Apodo</p>
-              <input type="text" name="nombre" placeholder="Tu Apodo" />
+              <input
+                type="text"
+                placeholder="Tu Apodo"
+                onChange={handleNickname}
+                maxLength="20"
+              />
+              <a
+                onSubmit={sendNicknameToBase(nickname)}
+                href="../partidas.html"
+              >
+                Jugar
+              </a>
             </form>
-            <button>Jugar</button>
           </section>
         </div>
       </header>
